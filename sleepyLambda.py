76c091fe96aeceb4a5b95be4cae29d3d24ede520
jmoe <james.moessis1@gmail.com>
2018-08-12 18:17:42 +1000

    Give appropriate response in JSON so Alexa doesnt cry

--- sleepyLambda.py
+++ sleepyLambda.py
@@ -5,8 +5,8 @@ def lambda_handler(event, context):
     client = boto3.client('sqs')
     response = client.send_message(
         QueueUrl='https://sqs.us-east-1.amazonaws.com/141291046059/sleepyQueue',
-        MessageBody='testHowdy folks',
+        MessageBody='Howdy from Lambda',
         DelaySeconds=0,
     )
     
-    return str(response['ResponseMetadata']['HTTPStatusCode'])
+    return {"response": {"howdy":"well dagnabbit jimmy how the hell are ya"}}
